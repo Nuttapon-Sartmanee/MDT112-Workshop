f129841e48c68e83bdc5958b36e2336f69c7a73e
Nuttapon_Sartmanee <nuttapon.a6@mail.kmutt.ac.th>
2021-03-07 01:33:38 +0700

    070364 add .cs file (Conditional Statement)

---
+++ 63120501026 Parabola.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Parabola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            //y = Math.Pow(x - 1, 2);
+            //x = Math.Sqrt(y) + 1;
+
+            switch (mode)
+            {
+                case "time":
+                    if (x > 0)
+                    {
+                        Console.WriteLine("{0},{1}", x , Math.Pow(x - 1, 2));
+                    }
+                    else 
+                                Console.WriteLine("Invalid mouse position");
+                    break;
+                
+                case "price":
+                    if (x > 0)
+                    {
+                        Console.WriteLine("{0},{1}", Math.Sqrt(y) + 1, y);
+                    }
+                    
+                    else
+                        Console.WriteLine("Invalid mouse position");
+                    break;
+                
+                default:
+                        Console.WriteLine("Invalid mode");
+                    break;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
