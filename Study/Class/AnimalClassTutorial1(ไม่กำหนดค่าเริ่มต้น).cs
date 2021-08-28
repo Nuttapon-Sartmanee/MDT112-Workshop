adfda233edc7d5a65663c891a3efb5a3596d1f29
Nuttapon_Sartmanee <nuttapon.a6@mail.kmutt.ac.th>
2021-08-28 12:15:23 +0700

    28/08/64 Lastfile for today

---
+++ Study/Class/AnimalClassTutorial1(ไม่กำหนดค่าเริ่มต้น).cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            Animal Rabbit = new Animal();
+
+            Rabbit.Name = "FLUKE";
+            Rabbit.Gender = "Female";
+            Rabbit.Birthday = "11/11/2544";
+
+            Console.WriteLine(Rabbit.Name);
+
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public string Name;
+        public string Gender;
+        public string Birthday;
+    }
+}
---
+++ Study/Class/StudentClassTutorial1(คลาส + เมธอด).cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            Student TEST = new Student();
+
+            TEST.Name = "Jane";
+            TEST.SurName = "Jane";
+            TEST.Grade = "A";
+
+            Console.WriteLine(TEST.Grade);
+            ChangeGrade(TEST);
+            Console.WriteLine(TEST.Grade);
+
+            Console.ReadKey();
+        }
+        static void ChangeGrade(Student TEST)
+        {
+            if (TEST.Grade == "A")
+            {
+                TEST.Grade = "F";
+            }
+        }
+
+        
+    }
+    class Student
+    {
+        public string Name;
+        public string SurName;
+        public string Grade;
+    }
+}
