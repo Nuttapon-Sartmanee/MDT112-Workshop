019fae2f12657eebdb6082b071516ff433e97f6a
Nuttapon_Sartmanee <nuttapon.a6@mail.kmutt.ac.th>
2021-08-28 10:48:50 +0700

    28/08/2564 GAME CLASS

---
+++ Study/Class/GameClassTutorial1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        
+        static void Main()
+        {
+            Swordman Arenaiz6 = new Swordman();
+            Console.WriteLine(Arenaiz6.HP);
+
+            Console.ReadKey();
+        }
+    }
+    class Swordman
+    {
+        public int HP;
+        public int EXP;
+        public int ATK;
+        public int DEF;
+
+        public Swordman()
+        {
+            HP = 300;
+            EXP = 0;
+            ATK = 1;
+            DEF = 1;
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("Walk");
+        }
+        public void Jump()
+        {
+            Console.WriteLine("Jump");
+        }
+        public void Sit()
+        {
+            Console.WriteLine("Sit");
+        }
+        public void Dead()
+        {
+            Console.WriteLine("Dead");
+        }
+        
+    }
+}
