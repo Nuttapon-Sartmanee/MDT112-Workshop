8000c6d52365d025b658f8f7300f37b94ed3410a
Nuttapon_Sartmanee <nuttapon.a6@mail.kmutt.ac.th>
2021-10-02 20:57:56 +0700

    กลางภาค(ไม่ได้ทำข้อ 2) 2/10/64

---
+++ (กลางภาค) 1.Hangman.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace Test
+{
+
+    class Program
+    {
+        static int IncorrectScore = 0;
+        static int CorrectScore = 0;
+        static int NumWord = 0;
+        static char[] Answer;
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu() //MENU
+        {
+           Console.Clear();
+           HeadStart();
+           InputMenu();
+        }
+
+        static void InputMenu()//INPUT PLAY/EXIT
+        {
+            Console.WriteLine("1. Play game");
+            Console.WriteLine("2. Exit");
+            int SelectMenu = int.Parse(Console.ReadLine());
+            if (SelectMenu == 1)
+            {
+                Gameplay();
+            }
+            else
+            {
+            }
+        }
+        static void Gameplay()//GAMEPLAY
+        {
+            Console.Clear();
+            char[] Word = GenerateWord(RandomWord());//RANDOM WORD
+            ShowWord(Word);
+        }
+        static void InputAndCheck(char[] Word)
+        {
+            char GameInput = InputLetter();//INPUT FROM PLAYER
+            CheckInput(GameInput, Word);
+        }
+
+        static void ShowWord(char[] Word)//PRINT INTERFACE
+        {
+            Console.Clear();
+            HeadGamePlay();
+            ShowIncorrectScore();
+            Console.WriteLine(Word);
+
+            if (CorrectScore >= NumWord)//CHECK FOR WIN
+            {
+                Console.Clear();
+                HeadGamePlay();
+                ShowIncorrectScore();
+                Console.WriteLine(Word);
+                Console.WriteLine("You win!");
+                Console.ReadKey();
+            }
+            else if (IncorrectScore >= 6)//CHECK FOR GAMEOVER
+            {
+                Console.Clear();
+                HeadGamePlay();
+                ShowIncorrectScore();
+                Console.WriteLine(Word);
+                Console.WriteLine("Game Over");
+                Console.ReadKey();
+            }
+            else
+            {
+                InputAndCheck(Word);
+            }
+        }
+        static char[] GenerateWord(WordList RandomWord)//RANDOM WORD
+        {
+            string NewWord = RandomWord.ToString();
+            NumWord = NewWord.Length;
+
+            Answer = NewWord.ToCharArray();
+            char[] Word = new char[NumWord]; 
+            for(int i = 0; i < NumWord ; i++)
+            {
+              Word[i] = '-';
+            }
+            return Word;
+        }
+        static void CheckInput(char Input , char[] Word)//CHECK ANSWER INPUT FROM PLAYER
+        {
+            int Incorrect = 0;
+            for (int i = 0 ; i < NumWord; i++ ) 
+            {
+                if (Input == Answer[i])
+                {
+                    Word[i] = Input;
+                    Incorrect = 0;
+                    CorrectScore++;
+                }
+                else 
+                {
+                    Incorrect++;
+                }
+            }
+            if (Incorrect > NumWord - 1)
+            {
+                IncorrectScore++;
+            }
+            ShowWord(Word);
+        }
+        static char InputLetter()//ANSWER INPUT
+        {
+            char Input;
+            Console.Write("Input letter alphabet: ");
+            Input = char.Parse(Console.ReadLine());
+            return Input;
+        }
+
+        enum WordList//ALL WORD
+        { 
+            Tennis = 0,
+            Football = 1,
+            Badminton = 2
+        }
+        static WordList RandomWord()//NUMBER TO WORD
+        {
+            int NumRandom = RandomNum();
+            WordList Word = (WordList)NumRandom;
+            return Word;
+        }
+        static int RandomNum()//RANDOM NUMBER
+        {
+            Random random = new Random();
+            int resultRandom = random.Next(0 , 3);
+            return resultRandom;
+        }
+        static void ShowIncorrectScore()//FOR PRINT
+        {
+            Console.WriteLine("Incorrect Score: {0}" , IncorrectScore);
+        }
+        static void HeadStart()//FOR PRINT
+        {
+            Console.WriteLine("Welcome to Hangman Game");
+            Console.WriteLine("----------------------------------------");
+        }
+        static void HeadGamePlay()//FOR PRINT
+        {
+            Console.WriteLine("Play Game Hangman");
+            Console.WriteLine("----------------------------------------");
+        }
+    }
+}
+
+
---
+++ (กลางภาค) 3.ปรับปรุงร้านดอกไม้.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlowerStore flowerStore = new FlowerStore();
+            Menu(flowerStore);
+        }
+        static void Menu(FlowerStore flowerStore)
+        {
+            PrintOrder(flowerStore);
+            SelectFlower(flowerStore);
+            selectExit(flowerStore);
+        }
+        static void selectExit(FlowerStore flowerStore)
+        {
+            string decide = "No";
+            Console.WriteLine("You can stop this progress ? exit for >> exit << progress and press any key for continue");
+            decide = Console.ReadLine();
+            if (decide == "exit")
+            {
+                ExitProgram(flowerStore);
+            }
+            else
+                Menu(flowerStore);
+        }
+        static void SelectFlower(FlowerStore flowerStore) 
+        {
+            string selectFlower;
+            selectFlower = Console.ReadLine();
+            switch (selectFlower)
+            {
+                case "1":
+                    flowerStore.addToCart(flowerStore.flowerList[0]);
+                    Console.WriteLine("Added " + flowerStore.flowerList[0]);
+                    break;
+                case "2":
+                    flowerStore.addToCart(flowerStore.flowerList[1]);
+                    Console.WriteLine("Added " + flowerStore.flowerList[1]);
+                    break;
+                default:
+                    Console.WriteLine("Not Added to cart. found select number of flower");
+                    break;
+            }
+        }
+        static void PrintOrder(FlowerStore flowerStore)
+        {
+            Console.WriteLine("Select number for buy flower :");
+            foreach (string i in flowerStore.flowerList)
+            {
+                Console.Write((flowerStore.flowerList.IndexOf(i) + 1) + " ");
+                Console.WriteLine(i);
+            }
+        }
+        static void ExitProgram(FlowerStore flowerStore)
+        {
+            Console.Write("Current my cart");
+            flowerStore.showCart();
+            Console.ReadKey();
+        }
+    }
+    class FlowerStore
+    {
+        public List<string> flowerList = new List<string>();
+        List<string> cart = new List<string>();
+        public FlowerStore()
+        {
+            flowerList.Add("Rose");
+            flowerList.Add("Lotus");
+        }
+        public void addToCart(string name)
+        {
+            cart.Add(name);
+        }
+
+        public void showCart()
+        {
+            if (cart.Count == 0)
+            {
+                Console.WriteLine("Cart is empty");
+            }
+            else
+            {
+                Console.WriteLine("My Cart :");
+                foreach (string i in cart)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+
+
