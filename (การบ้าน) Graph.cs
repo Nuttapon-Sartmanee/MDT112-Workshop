7c9dff6c7ccde960038b565e21599e1f18f64284
Nuttapon_Sartmanee <nuttapon.a6@mail.kmutt.ac.th>
2021-11-30 23:58:10 +0700

    การบ้านครั้งที่ 4 - Graph

---
+++ (การบ้าน) Graph.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Total of City: ");
+            int TotalCity = int.Parse(Console.ReadLine());
+            int[,] Graph = new int[TotalCity, TotalCity];
+
+            for (int i = 1; i <= TotalCity; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i != j) 
+                    {
+                        Graph[i - 1, j - 1] = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+
+            Console.WriteLine("Show Graph");
+            Console.WriteLine("");
+
+            for (int i = 1; i <= TotalCity; i++)
+            {
+                for (int j = 1; j <= TotalCity; j++)
+                {
+                    Console.Write(Graph[i - 1 , j - 1]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
